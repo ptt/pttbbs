03ff0419cbcbbf2fc8cb540b833e5860ecba995e
in2 <in2@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2002-09-20 15:04:04 +0000

    *** empty log message ***

---
+++ trunk/pttbbs/util/lunar-1,15run.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use LocalVars;
+# 每農曆初一, 十五就會跑一次 $ARGV[0]
+# 資料來源 http://tw.weathers.yahoo.com/
+open FH, "$LYNX -source http://tw.weathers.yahoo.com/ | grep '民國'|";
+$din = <FH>;
+close FH;
+
+($month, $day) = $din =~ /農曆 (.*?)月 (.*?)日/;
+system($ARGV[0]) if( $day eq '一' || $day eq '十五' );
