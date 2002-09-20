e2aa04075d49934b72a4e3ab5bdb87469f2601f9
in2 <in2@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2002-09-20 15:22:18 +0000

    fix bug

--- trunk/pttbbs/util/lunar-1,15run.pl
+++ trunk/pttbbs/util/lunar-1,15run.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use lib '/home/bbs/bin/';
 use LocalVars;
 # 每農曆初一, 十五就會跑一次 $ARGV[0]
 # 資料來源 http://tw.weathers.yahoo.com/
@@ -7,4 +8,4 @@ $din = <FH>;
 close FH;
 
 ($month, $day) = $din =~ /農曆 (.*?)月 (.*?)日/;
-system($ARGV[0]) if( $day eq '一' || $day eq '十五' );
+system("@ARGV") if( $day eq '一' || $day eq '十五' );
