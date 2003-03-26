8521e1f202e83ee131546930030f2698c403034a
in2 <in2@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2003-03-26 12:32:16 +0000

    FAVNOHILIGHT

--- trunk/pttbbs/mbbsd/board.c
+++ trunk/pttbbs/mbbsd/board.c
@@ -1,4 +1,4 @@
-/* $Id: board.c,v 1.95 2003/03/26 12:24:40 in2 Exp $ */
+/* $Id: board.c,v 1.96 2003/03/26 12:32:16 in2 Exp $ */
 #include "bbs.h"
 #define BRC_STRLEN 15		/* Length of board name */
 #define BRC_MAXSIZE     24576
@@ -931,8 +931,9 @@ show_brdlist(int head, int clsflag, int newflag)
 		}
 		if (class_bid != 1) {
 		    prints("%s%-13s\033[m%s%5.5s\033[0;37m%2.2s\033[m"
-			    "%-34.34s",
-			    (ptr->myattr & BRD_FAV) ? "\033[1;36m" : "",
+			   "%-34.34s",
+			   ((ptr->myattr & BRD_FAV) &&
+			    !cuser.uflag2 & FAVNOHILIGHT)? "\033[1;36m" : "",
 			    B_BH(ptr)->brdname,
 			    color[(unsigned int)
 			    (B_BH(ptr)->title[1] + B_BH(ptr)->title[2] +
