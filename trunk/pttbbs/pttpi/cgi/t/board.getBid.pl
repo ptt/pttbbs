f172423a53869bddfcb3403e36f894dcc918129f
in2 <in2@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2003-05-19 01:57:22 +0000

    rename 1.board.getBid.pl board.getBid.pl

--- trunk/pttbbs/pttpi/cgi/t/board.getBid.pl
+++ trunk/pttbbs/pttpi/cgi/t/board.getBid.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: 1.board.getBid.pl,v 1.2 2003/05/19 01:52:22 in2 Exp $
+# $Id: board.getBid.pl,v 1.1 2003/05/19 01:57:22 in2 Exp $
 use Frontier::Client;
 use Data::Dumper;
 do 'host.pl';
