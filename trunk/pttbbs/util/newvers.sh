7d46480b890a0bf68508d10fa691ea08ca3d4af8
in2 <in2@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2003-06-22 14:45:17 +0000

    change [] to *

--- trunk/pttbbs/util/newvers.sh
+++ trunk/pttbbs/util/newvers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# $Id: newvers.sh,v 1.1 2003/06/22 04:32:38 in2 Exp $
+# $Id: newvers.sh,v 1.2 2003/06/22 14:45:17 in2 Exp $
 
 t=`date`
 
 cat << EOF > vers.c
-char    compile_time[] = "${t}";
+char    *compile_time = "${t}";
 EOF
