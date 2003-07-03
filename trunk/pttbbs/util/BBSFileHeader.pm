17e957faaf7453ccfdd263bf58a9c0c549d39d45
in2 <in2@63ad8ddf-47c3-0310-b6dd-a9e9d9715204>
2003-07-03 05:49:06 +0000

    move BBSFileHeader.pm to util/

--- trunk/pttbbs/util/BBSFileHeader.pm
+++ trunk/pttbbs/util/BBSFileHeader.pm
@@ -44,11 +44,13 @@ sub FETCH
 	return `/bin/cat $fn`;
     }
     else{
-	($h{filename}, $h{recommend}, $h{owner}, $h{date}, $h{title}) =
-	    unpack('Z33cZ14Z6Z65', $t);
+	($h{filename}, $h{recommend}, $h{owner}, $h{date}, $h{title},
+	 $h{money}, undef, $h{filemode}) =
+	    unpack('Z33cZ14Z6Z65iCC', $t);
 	$h{title} = substr($h{title}, 3);
 	$self->{cache}{$num}{$_} = $h{$_}
-	    foreach( 'filename', 'owner', 'date', 'title' );
+	    foreach( 'filename', 'owner', 'date',
+		     'title', 'money', 'filemode' );
 	return $h{$key};
     }
 }
