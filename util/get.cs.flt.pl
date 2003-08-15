66bb039f55dcd111b21ce93a891a5cc0eb85713d
Victor Hsieh <victor@csie.org>
2003-08-15 10:40:13 +0000

    new developed service

--- util/get.cs.flt.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-@r = split(' ', $t = `/usr/bin/vmstat -n 0 -c 5 1`);
-open FH, '>/tmp/mrtg.cs';
-print FH (($r[52]+$r[69]+$r[86]+$r[103])/4);
-close FH;
-open FH, '>/tmp/mrtg.flt';
-print FH (($r[44]+$r[61]+$r[78]+$r[95])/4);
-close FH;
--- web/post.c
+++
@@ -1,6 +0,0 @@
-#include "mod_ptt.h"
-
-int bbs_post(request_rec *r, void *args)
-{
-   return OK;
-}
