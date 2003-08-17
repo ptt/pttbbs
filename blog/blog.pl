2557f59989a3810d4024cc3d366abcffaccc8012
in2 <in2@in2home.org>
2003-08-17 01:09:33 +0000

    param('searchboard')

--- blog/blog.pl
+++ blog/blog.pl
@@ -29,6 +29,17 @@ sub main
     @cnumber = ('零', '一', '二', '三', '四', '五', '六',
 		'七', '八', '九', '十', '十一', '十二');
     
+    if( $brdname = param('searchboard') ){
+        dodbi(sub {
+            my($dbh) = @_;
+            my($sth);
+            $sth = $dbh->prepare("select k from counter where k='$brdname'");
+            $sth->execute();
+            $brdname  = $sth->fetchrow_hashref()->{k};
+        });
+        return redirect("/blog.pl/$brdname/");
+    }
+    
     if( !$ENV{PATH_INFO} ){
         print header(-status => 400);
         return;
