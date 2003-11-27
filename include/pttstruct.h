ee8c2d8e36fc7e02b048a24cd11900f6aeb55345
Victor Hsieh <victor@csie.org>
2003-11-27 14:42:46 +0000

    revert

--- include/pttstruct.h
+++ include/pttstruct.h
@@ -99,7 +99,7 @@ typedef struct userec_t {
     unsigned char   goodsale;		/* 競標 好的評價  */
     unsigned char   badsale;		/* 競標 壞的評價  */
 
-    char    pad[77];
+    char    pad[67];
 } userec_t;
 /* these are flags in userec_t.uflag */
 #define PAGER_FLAG      0x4     /* true if pager was OFF last session */
