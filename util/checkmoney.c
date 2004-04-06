487adcc2153cafbb9c9ff06e05a0a829171a028b
Ethan Tu <ptt1.tu@gmail.com>
2004-04-06 16:55:18 +0000

    tools for fix money bug

--- util/checkmoney.c
+++ util/checkmoney.c
@@ -5,6 +5,7 @@
 
 int main(int argc, char*argv[]) {
     userec_t user;
+    char userhomepwd[256];
     char *userid;
     int i, money;
 
@@ -17,10 +18,22 @@ int main(int argc, char*argv[]) {
     for(i = 1; i <= MAX_USERS; i++) {
         userid=SHM->userid[i-1]; 
         money =SHM->money[i-1];
-	if(!userid[0]) continue;
+	if(!userid[0] || money==-1 ) continue;
         if(get_record(argv[1], &user, sizeof(user), i) == -1)continue;
-        printf("%s %d %s %d\n", userid, money, user.userid, user.money); 
-      
+	if(money-user.money > 1000000 ||( money<100 && user.money>2000)) 
+	{    
+	  sprintf(userhomepwd, BBSHOME "/%c/%s/.passwd", user.userid[0],
+		    user.userid);
+          printf("%s %d ", userid, money); 
+	  money= user.money;
+	  SHM->money[i-1]  = money; 
+	  if(get_record(userhomepwd, &user, sizeof(user), 1) == -1)
+	       get_record(BBSHOME"/.PASSWD",  &user, sizeof(user), i);     
+	  user.money = money;
+          printf("become %d\n",  money); 
+	  substitute_record(BBSHOME"/.PASSWD",  &user, sizeof(user), i);
+	  substitute_record(userhomepwd,  &user, sizeof(user), 1);
+	  }
     }
 
     return 0;
