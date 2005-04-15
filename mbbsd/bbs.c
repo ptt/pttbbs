f638192822a5b9450f8f616d276b5488c58abf11
Kuang-che Wu <kcwu@csie.org>
2005-04-15 14:51:23 +0000

    array index off by 1 bug

--- mbbsd/bbs.c
+++ mbbsd/bbs.c
@@ -262,7 +262,7 @@ whereami(int ent, const fileheader_t * fhdr, const char *direct)
     bh = getbcache(currutmp->brc_id);
     root = getbcache(1);
     p[0] = bh;
-    for (i = 0; i < WHEREAMI_LEVEL && p[i]->parent>1; i++)
+    for (i = 0; i+1 < WHEREAMI_LEVEL && p[i]->parent>1; i++)
 	p[i + 1] = getbcache(p[i]->parent);
     j = i;
     prints("§Ú¦b­þ?\n%-40.40s %.13s\n", p[j]->title + 7, p[j]->BM);
