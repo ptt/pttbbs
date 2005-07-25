0c3fc18175db1f6e957bc2236426c55a556ec711
Hung-Te Lin (piaip) <hungte@gmail.com>
2005-07-25 06:05:53 +0000

    mark as broken

--- util/rmuid.c
+++ util/rmuid.c
@@ -1,4 +1,7 @@
 /* $Id$ */
+#include <stdio.h>
+
+#if 0
 #include "bbs.h"
 
 extern int numboards;
@@ -11,14 +14,19 @@ int getbidofuid(int uid)
  for (n=0;n<numboards;n++)
     { 
 	bh = &bcache[n]; 
+	/* unused == uid!? don't use a variable claimed as 'unused!' */
 	if(bh->unused == uid)
 		 return n+1;
     }
  return 1;
 }
+#endif
 
 int main(int argc, char* argv[])
 {
+    fprintf(stderr, "This utility is marked as broken.\n");
+
+#if 0
     //struct stat  st;
     int n;
     boardheader_t bh;
@@ -32,6 +40,7 @@ int main(int argc, char* argv[])
 	//printf("%14.14s%14.14s \r\n",bh.brdname, bh.title);
 	substitute_record("BOARDS.bid", &bh, sizeof(bh), n+1);
     }
+#endif
     return 0;
 }
 
