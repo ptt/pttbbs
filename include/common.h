755b03cd78593e6d86f703b29b2a282caf6de4ea
Hung-Te Lin (piaip) <hungte@gmail.com>
2005-08-04 16:46:01 +0000

    help update

--- include/common.h
+++ include/common.h
@@ -49,9 +49,9 @@
 #define MSG_POSTER      ANSI_COLOR(34;46) " 文章選讀 "\
 	    ANSI_COLOR(31;47) " (y)"	ANSI_COLOR(30) "回信"\
 	    ANSI_COLOR(31) "(X)" 	ANSI_COLOR(30) "推文"\
-	    ANSI_COLOR(31) "(x)" 	ANSI_COLOR(30) "轉錄 "\
+	    ANSI_COLOR(31) "(x)" 	ANSI_COLOR(30) "轉錄  "\
 	    ANSI_COLOR(31) "(=[]<>)" 	ANSI_COLOR(30) "相關主題 "\
-	    ANSI_COLOR(31) "(/?aA)" 	ANSI_COLOR(30) "搜尋標題／作者 "\
+	    ANSI_COLOR(31) "(/?a)" 	ANSI_COLOR(30) "搜尋標題/作者  "\
 	    ANSI_COLOR(31) "(V)" 	ANSI_COLOR(30) "投票 "\
 	    ANSI_RESET ANSI_CLRTOEND
 #define MSG_SEPERATOR   "\
