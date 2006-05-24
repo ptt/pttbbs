24e1fcafea25a01a5f7dfe3c869e2560953039c8
Kuang-che Wu <kcwu@csie.org>
2006-05-24 16:04:28 +0000

    r3440@kcwu:  kcwu | 2006-05-25 00:04:16 +0800  fix two resource leak: fclose() not paired.

--- mbbsd/user.c
+++ mbbsd/user.c
@@ -171,6 +171,7 @@ user_display(const userec_t * u, int adminmode)
 		    fgets(genbuf, 200, fp);
 		fgets(genbuf, 200, fp);
 		prints("%12s´Ñ°ê¦Û§Ú´y­z: %s", chess_type[i], genbuf + 11);
+		fclose(fp);
 	    }
 	}
     }
@@ -1096,6 +1097,7 @@ showplans_userec(userec_t *user)
 
 		i++;
 	    }
+	    fclose(fp);
 
 	    if (user_query_mode == 1) {
 		win = user->five_win;
