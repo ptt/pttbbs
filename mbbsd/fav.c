31ea395c9e6cae2e95d84f4e3afa94b08900b1a1
Victor Hsieh <victor@csie.org>
2006-06-05 10:21:33 +0000

    fix a stupid bug

--- mbbsd/fav.c
+++ mbbsd/fav.c
@@ -448,7 +448,7 @@ static void read_favrec(FILE *frp, fav_t *fp)
 	switch (ft->type) {
 	    case FAVT_FOLDER:
 		fread(&cast_folder(ft)->fid, sizeof(char), 1, frp);
-		fread(&cast_folder(ft)->title, sizeof(BTLEN + 1), 1, frp);
+		fread(&cast_folder(ft)->title, BTLEN + 1, 1, frp);
 		break;
 	    case FAVT_BOARD:
 	    case FAVT_LINE:
@@ -547,7 +547,7 @@ static void write_favrec(FILE *fwp, fav_t *fp)
 	switch (ft->type) {
 	    case FAVT_FOLDER:
 		fwrite(&cast_folder(ft)->fid, sizeof(char), 1, fwp);
-		fwrite(&cast_folder(ft)->title, sizeof(BTLEN + 1), 1, fwp);
+		fwrite(&cast_folder(ft)->title, BTLEN + 1, 1, fwp);
 		break;
 	    case FAVT_BOARD:
 	    case FAVT_LINE:
