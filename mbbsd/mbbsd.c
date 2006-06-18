d97d00507b81f696eefd067e5195db9b7e7e3b40
in2 <in2@in2home.org>
2006-06-18 03:45:32 +0000

    make emacs happy

--- mbbsd/mbbsd.c
+++ mbbsd/mbbsd.c
@@ -1654,13 +1654,15 @@ daemon_login(int argc, char *argv[], char *envp[])
     /* main loop */
     while( 1 ){
 	len_of_sock_addr = sizeof(xsin);
+	if( 
 #if defined(Solaris) && __OS_MAJOR_VERSION__ == 5 && __OS_MINOR_VERSION__ < 7
-	if( (csock = accept(msock, (struct sockaddr *)&xsin,
-			    &len_of_sock_addr)) < 0 ){
+	   (csock = accept(msock, (struct sockaddr *)&xsin,
+			   &len_of_sock_addr)) < 0
 #else
-	if( (csock = accept(msock, (struct sockaddr *)&xsin,
-			    (socklen_t *)&len_of_sock_addr)) < 0 ){
+	   (csock = accept(msock, (struct sockaddr *)&xsin,
+			   (socklen_t *)&len_of_sock_addr)) < 0 
 #endif
+	   ) {
 	    if (errno != EINTR)
 		sleep(1);
 	    continue;
