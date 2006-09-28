619a5c297dac4dde0a10947ba463a64f2881ecf6
Chen-Yu Tsai <wens@csie.org>
2006-09-28 13:40:53 +0000

    damn backslash in dbcs

--- mbbsd/syspost.c
+++ mbbsd/syspost.c
@@ -136,7 +136,7 @@ post_policelog(const char *bname, const char *atitle, const char *action, const 
 
     snprintf(title, sizeof(title), "[%s] %s by %s", action, bname, cuser.userid);
     snprintf(genbuf, sizeof(genbuf),
-	     "%s (%s) %s %s 看板 %s 功能\n%s%s\n",
+	     "%s (%s) %s %s 看板 %s 功\能\n%s%s\n",
 	     cuser.userid, fromhost, toggle ? "開啟" : "關閉", bname, action,
 	     atitle ? "文章標題" : "", atitle ? atitle : "");
 
