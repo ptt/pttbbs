54daf88cb25a1c1f9dd70e81f60637c71b5b4528
Ethan Tu <ptt1.tu@gmail.com>
2007-01-10 20:25:24 +0000

    copy post's metadata (including money) while it is deleted.

--- mbbsd/bbs.c
+++ mbbsd/bbs.c
@@ -510,10 +510,10 @@ cancelpost(const fileheader_t *fh, int by_BM, char *newpath)
     if ((fin = fopen(fn1, "r"))) {
 	brd = by_BM ? "deleted" : "junk";
 
+        memcpy(&postfile, fh, sizeof(fileheader_t));
 	setbpath(newpath, brd);
 	stampfile(newpath, &postfile);
-	memcpy(postfile.owner, fh->owner, IDLEN + TTLEN + 10);
-
+	
 	nick[0] = '\0';
 	while (fgets(genbuf, sizeof(genbuf), fin)) {
 	    if (!strncmp(genbuf, str_author1, LEN_AUTHOR1) ||
