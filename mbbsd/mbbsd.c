65c89651ce80a998e6b9e18ce5b8dc0ed9d6748f
Hung-Te Lin (piaip) <hungte@gmail.com>
2008-01-05 08:23:29 +0000

    - remove extra ';'

--- mbbsd/mbbsd.c
+++ mbbsd/mbbsd.c
@@ -733,7 +733,7 @@ login_query(void)
 	    cuser.userlevel = 0;
 	    cuser.uflag = PAGER_FLAG | BRDSORT_FLAG | MOVIE_FLAG;
 
-#ifdef GUEST_DEFAULT_DBCS_NOINTRESC;
+#ifdef GUEST_DEFAULT_DBCS_NOINTRESC
 	    cuser.uflag |= DBCS_NOINTRESC;
 #endif
 	    // can we prevent mkuserdir() here?
