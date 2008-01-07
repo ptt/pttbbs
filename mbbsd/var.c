0a0d0b126b5aad1041a4b697bc1d4d1d240c87f8
Hung-Te Lin (piaip) <hungte@gmail.com>
2008-01-07 15:54:16 +0000

    - since there's nothing to use for non-reg-ok users in menu 'P', disable it.

--- mbbsd/var.c
+++ mbbsd/var.c
@@ -615,7 +615,7 @@ const commands_t      cmdlist[] = {
     {Talk, 	0,		"TTalk         【 休閒聊天區 】"},
     {User, 	PERM_BASIC,	"UUser         【 個人設定區 】"},
     {Xyz, 	0,		"XXyz          【 系統工具區 】"},
-    {Play_Play, PERM_BASIC, 	"PPlay         【 娛樂與休閒 】"},
+    {Play_Play, PERM_LOGINOK, 	"PPlay         【 娛樂與休閒 】"},
     {Name_Menu, PERM_LOGINOK,	"NNamelist     【 編特別名單 】"},
 #ifdef DEBUG
     {Goodbye, 	0, 		"GGoodbye      再見再見再見再見"},
