e043e9fb84ad25b9f2da76d6d8c73e49dd7ec300
Hung-Te Lin (piaip) <hungte@gmail.com>
2008-03-17 17:46:34 +0000

    - register: account expiring: fix offset-by-one issue.

--- mbbsd/register.c
+++ mbbsd/register.c
@@ -551,7 +551,8 @@ new_register(void)
 	if (bad_user_id(passbuf))
 	    outs("無法接受這個代號，請使用英文字母，並且不要包含空格\n");
 	else if ((id = getuser(passbuf, &xuser)) &&
-		 (minute = check_and_expire_account(id, &xuser)) > 0) 
+		// >=: see check_and_expire_account definition
+		 (minute = check_and_expire_account(id, &xuser)) >= 0) 
 	{
 	    if (minute == 999999) // XXX magic number.  It should be greater than MAX_USERS at least.
 		outs("此代號已經有人使用 是不死之身");
