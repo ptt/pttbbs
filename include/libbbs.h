60b64c6b942a849d143c5308ee32306b1f053cd5
Hung-Te Lin (piaip) <hungte@gmail.com>
2008-03-19 18:26:49 +0000

    (internal) prepare for releasing in new layout

--- include/libbbs.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LIBBBS_H_
-#define _LIBBBS_H_
-
-extern int give_tax(int money);
-extern const char* money_level(int money);
-
-#endif
--- include/util.h
+++
@@ -1,5 +0,0 @@
-#ifndef INCLUDE_UTIL_H
-#define INCLUDE_UTIL_H
-int passwd_mmap(void);
-
-#endif // INCLUDE_UTIL_H
