eccfcf5ee966e0197ab6db7c8506e4918f4d468f
Chen-Yu Tsai <wens@csie.org>
2008-04-10 04:19:54 +0000

    Missed this in last commit

--- daemon/fromd/fromc.c
+++ daemon/fromd/fromc.c
@@ -15,21 +15,16 @@ int main(int argc, char *argv[])
     memset(buf, 0, sizeof(buf));
 
     if (argc < 4) {
-	fprintf(stderr, "Usage: %s ip port lookup_ip\n", argv[0]);
+	fprintf(stderr, "Usage: %s ip:port lookup_ip\n", argv[0]);
 	return 0;
     }
 
-    if ( (port = atoi(argv[2])) == 0 ) {
-	fprintf(stderr, "Port given is not valid\n");
-	return 1;
-    }
-
-    if ( (fd = toconnect(argv[1], port)) < 0 ) {
+    if ( (fd = toconnect(argv[1])) < 0 ) {
 	perror("toconnect");
 	return 1;
     }
 
-    write(fd, argv[3], strlen(argv[3]));
+    write(fd, argv[2], strlen(argv[2]));
     read(fd, buf, sizeof(buf));
 
     printf("%s\n", buf);
