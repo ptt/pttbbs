b77efc4c3bd0d3cf2b2d8cf12128dfa46dff997a
Chen-Yu Tsai <wens@csie.org>
2008-06-12 03:50:50 +0000

    Clean up makefiles

--- pttbbs.mk
+++ pttbbs.mk
@@ -5,35 +5,24 @@ BBSHOME?=	/home/bbs
 
 SRCROOT?=	.
 
-OS!=		uname
-OS_MAJOR_VER!=	uname -r|cut -d . -f 1
-OS_MINOR_VER!=	uname -r|cut -d . -f 2
-OSTYPE?=	$(OS)
+OSTYPE!=	uname
 
 CC=		gcc
 CCACHE!=	which ccache|sed -e 's/^.*\///'
-PTT_CFLAGS=	-Wall -pipe -DBBSHOME='"$(BBSHOME)"' -I$(SRCROOT)/include
-PTT_LDFLAGS=	-L/usr/local/lib
-PTT_LIBS=	-lhz
+
+PTT_CFLAGS:=	-Wall -pipe -DBBSHOME='"$(BBSHOME)"' -I$(SRCROOT)/include
+PTT_LDFLAGS=
+PTT_LDLIBS=	-lhz
 
 # enable assert()
 #PTT_CFLAGS+=	-DNDEBUG 
 
+.if "$(OS)" == "FreeBSD"
 # FreeBSD特有的環境
-CFLAGS_FreeBSD=	-I/usr/local/include
-LDFLAGS_FreeBSD=
-LIBS_FreeBSD=	-lkvm -liconv
-
-# Linux特有的環境
-CFLAGS_Linux=	
-LDFLAGS_Linux=	
-LIBS_Linux=	
-
-# CFLAGS, LDFLAGS, LIBS 加入 OS 相關參數
-PTT_CFLAGS+=	$(CFLAGS_$(OSTYPE))
-PTT_LDFLAGS+=	$(LDFLAGS_$(OSTYPE))
-PTT_LIBS+=	$(LIBS_$(OSTYPE))
-
+PTT_CFLAGS+=  -I/usr/local/include
+PTT_LDFLAGS+= -L/usr/local/lib
+PTT_LDLIBS+=   -lkvm -liconv
+.endif
 
 # 若有定義 PROFILING
 .if defined(PROFILING)
@@ -50,18 +39,14 @@ GDB=		1
 PTT_CFLAGS+=	-DDEBUG 
 .endif
 
-.if defined(USE_ICC)
-CC=		icc
-CFLAGS=		$(PTT_CFLAGS) -O1 -tpp6 -mcpu=pentiumpro -march=pentiumiii \
-		-ip -ipo
-LDFLAGS+=	-O1 -tpp6 -mcpu=pentiumpro -march=pentiumiii -ip -ipo \
-		$(PTT_LDFLAGS) $(PTT_LIBS)
-.elif defined(GDB)
-CFLAGS=		-g -O0 $(PTT_CFLAGS)
-LDFLAGS=	-O0 $(PTT_LDFLAGS) $(PTT_LIBS)
+.if defined(GDB)
+CFLAGS:=	-g -O0 $(PTT_CFLAGS)
+LDFLAGS:=	-O0 $(PTT_LDFLAGS)
+LDLIBS:=	$(PTT_LDLIBS)
 .else
-CFLAGS+=	-g -Os $(PTT_CFLAGS) $(EXT_CFLAGS)
-LDFLAGS+=	-Os $(PTT_LDFLAGS) $(PTT_LIBS)
+CFLAGS:=	-g -Os $(PTT_CFLAGS) $(EXT_CFLAGS)
+LDFLAGS:=	-Os $(PTT_LDFLAGS)
+LDLIBS:=	$(PTT_LDLIBS)
 
 .if defined(OMITFP)
 CFLAGS+=	-fomit-frame-pointer
