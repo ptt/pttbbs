ad4ed384de8f6a4f8ca20924e275228ebdca477d
Chen-Yu Tsai <wens@csie.org>
2008-06-19 08:06:32 +0000

    Add header files

--- common/bbs/fhdr_stamp.c
+++ common/bbs/fhdr_stamp.c
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include "cmsys.h"  // for time4_t
 #include "cmbbs.h"
 
