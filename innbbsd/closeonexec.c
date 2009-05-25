fdd07a28c90c06d070322de026cf18de8dd269a3
Kuang-che Wu <kcwu@csie.org>
2009-05-25 03:56:59 +0000

    - remove dead code (closeOnExec)

--- innbbsd/closeonexec.c
+++ innbbsd/closeonexec.c
@@ -9,40 +9,10 @@
 #include <sys/ioctl.h>
 #include "clibrary.h"
 
-#ifndef CLX_IOCTL
-#define CLX_IOCTL
-#endif
 #ifndef CLX_FCNTL
 #define CLX_FCNTL
 #endif
 
-
-
-
-#if	defined(CLX_IOCTL) && !defined(IRIX)
-#ifdef __linux
-#include <termios.h>
-#else
-#include <sgtty.h>
-#endif
-
-
-/*
- * *  Mark a file close-on-exec so that it doesn't get shared with our *
- * children.  Ignore any error codes.
- */
-void
-closeOnExec(fd, flag)
-    int             fd;
-    int             flag;
-{
-    int             oerrno;
-
-    oerrno = errno;
-    (void)ioctl(fd, flag ? FIOCLEX : FIONCLEX, (char *)NULL);
-    errno = oerrno;
-}
-#endif				/* defined(CLX_IOCTL) */
 
 
 
@@ -63,7 +33,7 @@ CloseOnExec(fd, flag)
     int             oerrno;
 
     oerrno = errno;
-    (void)fcntl(fd, F_SETFD, flag ? 1 : 0);
+    (void)fcntl(fd, F_SETFD, flag ? FD_CLOEXEC : 0);
     errno = oerrno;
 }
 #endif				/* defined(CLX_FCNTL) */
