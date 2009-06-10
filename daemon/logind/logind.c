81d1d01eb8f50c62d495789b27553e45732587a4
Hung-Te Lin (piaip) <hungte@gmail.com>
2009-06-10 01:45:30 +0000

    * support non-ascii in passwords(backward compatibility)

--- daemon/logind/logind.c
+++ daemon/logind/logind.c
@@ -178,7 +178,9 @@ login_ctx_handle(login_ctx *ctx, int c)
                 return LOGIN_HANDLE_WAIT;
             }
 
-            if (!isascii(c) || !isprint(c) || 
+            // XXX for backward compatibility,
+            // we'd support non-ASCII characters in PW.
+            if ( (isascii(c) && !isprint(c)) || 
                 l+1 >= sizeof(ctx->passwd))
                 return LOGIN_HANDLE_BEEP;
 
