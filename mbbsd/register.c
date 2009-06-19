f8afc7cb4fe3072871543b4a87fc7c7fd4523568
Hung-Te Lin (piaip) <hungte@gmail.com>
2009-06-19 16:27:07 +0000

    * clear previous messages

--- mbbsd/register.c
+++ mbbsd/register.c
@@ -1221,9 +1221,9 @@ toregister(char *email)
 		outs("但注意手動認證通常會花上數週以上的時間。\n");
 	    } else {
 #endif
-	    move(18, 0);
+	    move(17, 0);
 	    outs(ANSI_COLOR(1;31) 
-	    "提醒您: 如果之後發現您輸入的註冊資料有問題，不僅註冊會被取消，\n"
+	    "\n提醒您: 如果之後發現您輸入的註冊資料有問題，不僅註冊會被取消，\n"
 	    "原本認證用的 E-mail 也不能再用來認證。\n" ANSI_RESET);
 	    getdata(16, 0, "請再次確認您輸入的 E-Mail 位置正確嘛? [y/N]",
 		    yn, sizeof(yn), LCECHO);
