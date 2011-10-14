cf87db98fa9214cc75bde1f5560fd521c0dbd921
Hung-Te Lin (piaip) <hungte@gmail.com>
2011-10-14 16:58:29 +0000

    why lost this one?

--- mbbsd/menu.c
+++ mbbsd/menu.c
@@ -664,7 +664,6 @@ static const commands_t maillist[] = {
 // #ifdef USE_MAIL_AUTO_FORWARD
     {setforward, PERM_LOGINOK,  "FForward       設定信箱自動轉寄" },
 // #endif
-    {m_sysop, 0,                "YYes, sir!     寫信給站長"},
     {mail_mbox, PERM_INTERNET,  "RZip UserHome  把所有私人資料打包回去"},
     {built_mail_index, 
 	PERM_LOGINOK,		"SSavemail      重建信箱索引"},
