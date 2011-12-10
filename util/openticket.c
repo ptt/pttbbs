22574f4a20f7a7edc1ec8c6aa3356cf127614731
Hung-Te Lin (piaip) <hungte@gmail.com>
2011-12-10 09:33:04 +0000

    improve openticket results since items are random now.

--- util/openticket.c
+++ util/openticket.c
@@ -206,15 +206,15 @@ int main()
 	}
 
 	printf("\n\n開獎時間: %s\n\n"
-	       "開獎結果: %s\n\n"
+	       "開獎結果: %d. %s\n\n"
 	       "下注總額: %d00\n"
 	       "中獎比例: %d張/%d張  (%f)\n"
 	       "每張中獎彩票可得 %d " MONEYNAME "\n\n",
-	       Cdatelite(&now), betname[bet], total, ticket[bet], total,
+	       Cdatelite(&now), bet + 1, betname[bet], total, ticket[bet], total,
 	       (float) ticket[bet] / total, money);
 
-	fprintf(fp, "%s 開出:%s 總額:%d00 彩金/張:%d 機率:%1.2f\n",
-		Cdatelite(&now), betname[bet], total, money,
+	fprintf(fp, "%s 開出:%d.%s 總額:%d00 彩金/張:%d 機率:%1.2f\n",
+		Cdatelite(&now), bet + 1, betname[bet], total, money,
 		(float) ticket[bet] / total);
 	fclose(fp);
 
