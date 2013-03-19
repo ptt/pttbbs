285c9577df950b62e436831c5e3c11e2dcc9bd90
Hung-Te Lin (piaip) <hungte@gmail.com>
2013-03-19 16:50:31 +0000

    No money in FILE_POST articles.  #1HI2y7MN (PttBug) [ptt.cc]

--- mbbsd/bbs.c
+++ mbbsd/bbs.c
@@ -39,7 +39,8 @@ enum {
  * please help verify and finish these.
  */
 /* modes to invalid multi.money */
-#define INVALIDMONEY_MODES (FILE_ANONYMOUS | FILE_BOTTOM | FILE_DIGEST | FILE_BID)
+#define INVALIDMONEY_MODES (FILE_ANONYMOUS | FILE_BOTTOM | FILE_DIGEST | \
+                            FILE_VOTE | FILE_BID)
 
 /**
  * Test if the fhdr looks like really belong to user
