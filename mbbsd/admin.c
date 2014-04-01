20743e569fb1758424b2f6a68ddd3131e8608261
Hung-Te Lin (piaip) <hungte@gmail.com>
2014-04-01 01:45:37 +0000

    Revise messages

--- mbbsd/admin.c
+++ mbbsd/admin.c
@@ -246,7 +246,7 @@ search_key_user(const char *passwdfile, int mode)
 		if (ch == 's' && !mode) {
 		    if (retrieve_backup(&user) >= 0) {
 			fclose(fp1);
-                        vmsg("已成功\取用備份資料。\n");
+                        vmsg("已成功\取用備份資料。");
 			return 0;
 		    }
 		}
--- mbbsd/user.c
+++ mbbsd/user.c
@@ -210,7 +210,7 @@ user_display(const userec_t * u, int adminmode)
         prints("\tㄏノ癘魁: " STR_LOGINDAYS " %d " STR_LOGINDAYS_QTY
                ,u->numlogindays);
         prints(" / ゅ彻 %d 絞\n", u->numposts);
-        prints("\t程絬: %s (本–ら笆糤), ㄓ %s)\n",
+        prints("\t程絬: %s (本–ら糤) / %s\n",
                Cdate(&u->lastlogin), u->lasthost);
     } else {
 	diff = (now - login_start_time) / 60;
