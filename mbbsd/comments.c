fdedb624253732e8364568b2e48d5ba69e4476e0
Robert Wang <robertabcd@gmail.com>
2014-08-13 17:04:29 +0000

    Fix build breakage when not using commentd.

--- mbbsd/comments.c
+++ mbbsd/comments.c
@@ -266,10 +266,10 @@ int CommentsDeleteFromTextFile(void *ctx, int i, const char *reason)
     }
     return 0;
 }
-#endif
 
 const struct CommentKeyReq *CommentsGetKeyReq(void *ctx)
 {
     CommentsCtx *c = (CommentsCtx *)ctx;
     return &c->key;
 }
+#endif
