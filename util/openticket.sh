29bf8c2316d6aaf54c3213acdcd5209505db57ae
Hung-Te Lin (piaip) <hungte@gmail.com>
2014-09-16 11:30:44 +0000

    Revise 'gamble' messages.

--- util/openticket.sh
+++ util/openticket.sh
@@ -2,4 +2,4 @@
 # $Id: openticket.sh,v 1.1 2002/03/07 15:13:46 in2 Exp $
 
 bin/openticket > etc/jackpot
-bin/post Record "彩券中獎名單" "[賭場報告]" etc/jackpot
+bin/post Record "彩券中獎名單" "[派彩報告]" etc/jackpot
