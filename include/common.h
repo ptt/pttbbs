ptt/pttbbs

commit f75e71c52dc0a05a5e388ecc46c4ddfc96a5fe07

Fix potential bad memory access
Note, swater[5] was always NULL. After this fix, users can use one more
water line.

include/common.h
@@ -191,11 +191,10 @@
 /* 水球模式 邊界定義                                     */
 /* ----------------------------------------------------- */
 #define WB_OFO_USER_TOP		7
-#define WB_OFO_USER_BOTTOM	11
-#define WB_OFO_USER_HEIGHT	((WB_OFO_MSG_BOTTOM) - (WB_OFO_MSG_TOP) + 1)
 #define WB_OFO_USER_LEFT	28
 #define WB_OFO_MSG_TOP		15
 #define WB_OFO_MSG_BOTTOM	22
+#define WB_OFO_MSG_NUM		((WB_OFO_MSG_BOTTOM) - (WB_OFO_MSG_TOP) - 1)
 #define WB_OFO_MSG_LEFT		4
 
 /* ----------------------------------------------------- */

mbbsd/mbbsd.c
@@ -448,7 +448,9 @@ add_history(const msgque_t * msg)
     if (PAGER_UI_IS(PAGER_UI_ORIG) || PAGER_UI_IS(PAGER_UI_NEW))
 	add_history_water(&water[0], msg);
     if (PAGER_UI_IS(PAGER_UI_NEW) || PAGER_UI_IS(PAGER_UI_OFO)) {
-	for (i = 0; i < 5 && swater[i]; i++)
+	for (i = 0; i < WB_OFO_MSG_NUM; i++) {
+	    if (swater[i] == NULL)
+		break;
 	    if (swater[i]->pid == msg->pid
 #ifdef PLAY_ANGEL
 		    && swater[i]->msg[0].msgmode == msg->msgmode
@@ -456,10 +458,11 @@ add_history(const msgque_t * msg)
 #endif
 	       	)
 		break;
-	if (i == 5) {
+	}
+	if (i == WB_OFO_MSG_NUM) {
 	    waterinit = 1;
-	    i = 4;
-	    memset(swater[4], 0, sizeof(water_t));
+	    i = WB_OFO_MSG_NUM - 1;
+	    memset(swater[i], 0, sizeof(water_t));
 	} else if (!swater[i]) {
 	    water_usies = i + 1;
 	    swater[i] = &water[i + 1];

mbbsd/talk.c
@@ -625,7 +625,7 @@ ofo_my_write(void)
 #endif
     mvouts(WB_OFO_USER_TOP, WB_OFO_USER_LEFT,
            ANSI_COLOR(1;33;46) " ↑ 水球反擊對象 ↓" ANSI_RESET);
-    for (i = 0; i < WB_OFO_USER_HEIGHT;++i)
+    for (i = 0; i < WB_OFO_MSG_NUM;++i)
 	if (swater[i] == NULL || swater[i]->pid == 0)
 	    break;
 	else {
@@ -652,6 +652,7 @@ ofo_my_write(void)
 	case Ctrl('T'):
 	case KEY_UP:
 	    if (water_usies != 1) {
+		assert(0 < water_usies && water_usies <= WB_OFO_MSG_NUM);
 		ofo_water_scr(swater[(int)which], which, 0);
 		which = (which - 1 + water_usies) % water_usies;
 		ofo_water_scr(swater[(int)which], which, 1);
@@ -662,6 +663,7 @@ ofo_my_write(void)
 	case KEY_DOWN:
 	case Ctrl('R'):
 	    if (water_usies != 1) {
+		assert(0 < water_usies && water_usies <= WB_OFO_MSG_NUM);
 		ofo_water_scr(swater[(int)which], which, 0);
 		which = (which + 1 + water_usies) % water_usies;
 		ofo_water_scr(swater[(int)which], which, 1);

mbbsd/var.c
@@ -415,7 +415,7 @@ char            water_usies = 0;
 char            is_first_login_of_today = 0;
 char            is_login_ready = 0;
 FILE           *fp_writelog = NULL;
-water_t         *water, *swater[6], *water_which;
+water_t         *water, *swater[WB_OFO_MSG_NUM], *water_which;
 
 /* chc_play.c */
 

mbbsd/io.c
@@ -293,11 +293,8 @@ process_pager_keys(int ch)
 		break;
 
 	    check_water_init();
-	    if (water_which_flag == (int)water_usies)
-		water_which_flag = 0;
-	    else
-		water_which_flag =
-		    (water_which_flag + 1) % (int)(water_usies + 1);
+	    water_which_flag =
+		(water_which_flag + 1) % (int)(water_usies + 1);
 	    if (water_which_flag == 0)
 		water_which = &water[0];
 	    else

commit 5b9bf052c42fd7597bb3cb0ff67fb9cbe92b7775

Fix out-of-bound access on variable water.
Bug introduced by f75e71c52dc0a05a5e388ecc46c4ddfc96a5fe07.

include/common.h
@@ -191,10 +191,11 @@
 /* 水球模式 邊界定義                                     */
 /* ----------------------------------------------------- */
 #define WB_OFO_USER_TOP		7
+#define WB_OFO_USER_BOTTOM	11
+#define WB_OFO_USER_NUM		((WB_OFO_USER_BOTTOM) - (WB_OFO_USER_TOP) + 1)
 #define WB_OFO_USER_LEFT	28
 #define WB_OFO_MSG_TOP		15
 #define WB_OFO_MSG_BOTTOM	22
-#define WB_OFO_MSG_NUM		((WB_OFO_MSG_BOTTOM) - (WB_OFO_MSG_TOP) - 1)
 #define WB_OFO_MSG_LEFT		4
 
 /* ----------------------------------------------------- */

mbbsd/mbbsd.c
@@ -448,7 +448,7 @@ add_history(const msgque_t * msg)
     if (PAGER_UI_IS(PAGER_UI_ORIG) || PAGER_UI_IS(PAGER_UI_NEW))
 	add_history_water(&water[0], msg);
     if (PAGER_UI_IS(PAGER_UI_NEW) || PAGER_UI_IS(PAGER_UI_OFO)) {
-	for (i = 0; i < WB_OFO_MSG_NUM; i++) {
+	for (i = 0; i < WB_OFO_USER_NUM; i++) {
 	    if (swater[i] == NULL)
 		break;
 	    if (swater[i]->pid == msg->pid
@@ -459,9 +459,9 @@ add_history(const msgque_t * msg)
 	       	)
 		break;
 	}
-	if (i == WB_OFO_MSG_NUM) {
+	if (i == WB_OFO_USER_NUM) {
 	    waterinit = 1;
-	    i = WB_OFO_MSG_NUM - 1;
+	    i = WB_OFO_USER_NUM - 1;
 	    memset(swater[i], 0, sizeof(water_t));
 	} else if (!swater[i]) {
 	    water_usies = i + 1;

mbbsd/talk.c
@@ -541,8 +541,8 @@ static char     t_last_write[80];
 void check_water_init(void)
 {
     if(water==NULL) {
-	water = (water_t*)malloc(sizeof(water_t)*6);
-	memset(water, 0, sizeof(water_t)*6);
+	water = (water_t*)malloc(sizeof(water_t) * (WB_OFO_USER_NUM + 1));
+	memset(water, 0, sizeof(water_t) * (WB_OFO_USER_NUM + 1));
 	water_which = &water[0];
 
 	strlcpy(water[0].userid, " 全部 ", sizeof(water[0].userid));
@@ -625,7 +625,7 @@ ofo_my_write(void)
 #endif
     mvouts(WB_OFO_USER_TOP, WB_OFO_USER_LEFT,
            ANSI_COLOR(1;33;46) " ↑ 水球反擊對象 ↓" ANSI_RESET);
-    for (i = 0; i < WB_OFO_MSG_NUM;++i)
+    for (i = 0; i < WB_OFO_USER_NUM; ++i)
 	if (swater[i] == NULL || swater[i]->pid == 0)
 	    break;
 	else {
@@ -652,7 +652,7 @@ ofo_my_write(void)
 	case Ctrl('T'):
 	case KEY_UP:
 	    if (water_usies != 1) {
-		assert(0 < water_usies && water_usies <= WB_OFO_MSG_NUM);
+		assert(0 < water_usies && water_usies <= WB_OFO_USER_NUM);
 		ofo_water_scr(swater[(int)which], which, 0);
 		which = (which - 1 + water_usies) % water_usies;
 		ofo_water_scr(swater[(int)which], which, 1);
@@ -663,7 +663,7 @@ ofo_my_write(void)
 	case KEY_DOWN:
 	case Ctrl('R'):
 	    if (water_usies != 1) {
-		assert(0 < water_usies && water_usies <= WB_OFO_MSG_NUM);
+		assert(0 < water_usies && water_usies <= WB_OFO_USER_NUM);
 		ofo_water_scr(swater[(int)which], which, 0);
 		which = (which + 1 + water_usies) % water_usies;
 		ofo_water_scr(swater[(int)which], which, 1);

mbbsd/var.c
@@ -415,7 +415,7 @@ char            water_usies = 0;
 char            is_first_login_of_today = 0;
 char            is_login_ready = 0;
 FILE           *fp_writelog = NULL;
-water_t         *water, *swater[WB_OFO_MSG_NUM], *water_which;
+water_t         *water, *swater[WB_OFO_USER_NUM], *water_which;
 
 /* chc_play.c */
 