20ee6df5f904e45548c8a6410a46a111ce742a81
Kuang-che Wu <kcwu@csie.org>
2016-06-06 20:20:45 +0800

    revise path handling

--- util/deluserfile.c
+++ util/deluserfile.c
@@ -4,73 +4,75 @@
 
 #define HOLDWRITELOG
 #define DELZEROFILE
-#define USERHOME BBSHOME "/home"
 
 void del_file(char *userid)
 {
-    char buf[200], buf1[200];
+    char user_home[PATHLEN];
     struct dirent *de;
     DIR *dirp;
-    char *ptr;
 
-    sprintf(buf, BBSHOME "/home/%c/%s", userid[0], userid);
+    sethomepath(user_home, userid);
 
-    if (chdir(buf) == -1)
-	return;
-
-    if (!(dirp = opendir(buf)))
+    if (!(dirp = opendir(user_home)))
 	return;
 
     while ((de = readdir(dirp)))
     {
-	ptr = de->d_name;
-	if (ptr[0] > ' ' && ptr[0] != '.')
+	char *fname = de->d_name;
+	if (fname[0] > ' ' && fname[0] != '.')
 	{
-	    if (strstr(ptr, "writelog"))
+	    char user_file[PATHLEN];
+	    sethomefile(user_file, userid, fname);
+	    if (strstr(fname, "writelog"))
 #ifdef HOLDWRITELOG
 	    {
 		fileheader_t mymail;
+		char new_filename[PATHLEN];
+		char log_filename[PATHLEN];
+		char dir_filename[PATHLEN];
 
-		stampfile(buf, &mymail);
+		strcpy(new_filename, user_home);
+		stampfile(new_filename, &mymail);
 		mymail.filemode = FILE_READ;
 		strcpy(mymail.owner, "[備.忘.錄]");
 		strcpy(mymail.title, "熱線記錄");
-		sprintf(buf1, BBSHOME "/home/%c/%s/writelog",
-			userid[0], userid);
-		rename(buf1, buf);
-		sprintf(buf1, BBSHOME "/home/%c/%s/.DIR", userid[0], userid);
-		append_record(buf1, &mymail, sizeof(mymail));
+
+		sethomefile(log_filename, userid, "writelog");
+		rename(log_filename, new_filename);
+		sethomedir(dir_filename, userid);
+		append_record(dir_filename, &mymail, sizeof(mymail));
 	    }
 #else
-	    unlink(ptr);
+	    unlink(user_file);
 #endif
-	    else if (strstr(ptr, "chat_"))
-		unlink(ptr);
-	    else if (strstr(ptr, "ve_"))
-		unlink(ptr);
-	    else if (strstr(ptr, "SR."))
-		unlink(ptr);
-	    else if (strstr(ptr, ".old"))
-		unlink(ptr);
-	    else if (strstr(ptr, "talk_"))
-		unlink(ptr);
+	    else if (strstr(fname, "chat_"))
+		unlink(user_file);
+	    else if (strstr(fname, "ve_"))
+		unlink(user_file);
+	    else if (strstr(fname, "SR."))
+		unlink(user_file);
+	    else if (strstr(fname, ".old"))
+		unlink(user_file);
+	    else if (strstr(fname, "talk_"))
+		unlink(user_file);
 	}
     }
     closedir(dirp);
 }
 
-void mv_user_home(char *ptr)
+void mv_user_home(char *userid)
 {
-    char buf[200];
+    char command[PATHLEN*2];
+    char user_home[PATHLEN];
 
-    printf("move user %s to tmp\n", ptr);
-    sprintf(buf, "cp -R " BBSHOME "/home/%c/%s " BBSHOME "/tmp", ptr[0], ptr);
-//      sprintf(buf,"rm -rf " BBSHOME "/home/%c/%s",ptr[0],ptr);
-    if (!system(buf))
+    printf("move user %s to tmp\n", userid);
+    sethomepath(user_home, userid);
+    sprintf(command, "cp -R %s tmp", user_home);
+    if (!system(command))
     {				//Copy success
 
-	sprintf(buf, "rm -rf " BBSHOME "/home/%c/%s", ptr[0], ptr);
-	system(buf);
+	sprintf(command, "rm -rf %s", user_home);
+	system(command);
     }
 }
 
@@ -78,20 +80,21 @@ int main(void)
 {
     struct dirent *de;
     DIR *dirp;
-    char *ptr, buf[200], ch;
+    char *userid, buf[200], ch;
     int count = 0;
 /* visit all users  */
 
     printf("new version, deleting\n");
 
     attach_SHM();
+    chdir(BBSHOME);
 
     for (ch = 'A'; ch <= 'z'; ch++)
     {
 	if(ch > 'Z' && ch < 'a')
 	    continue;
 	printf("Cleaning %c\n", ch);
-	sprintf(buf, USERHOME "/%c", ch);
+	sprintf(buf, BBSHOME "/home/%c", ch);
 	if (!(dirp = opendir(buf)))
 	{
 	    printf("unable to open %s\n", buf);
@@ -100,17 +103,19 @@ int main(void)
 
 	while ((de = readdir(dirp)))
 	{
-	    ptr = de->d_name;
+	    userid = de->d_name;
 
 	    /* 預防錯誤 */
-	    if (is_validuserid(ptr))
+	    if (is_validuserid(userid))
 	    {
 		if (!(count++ % 300))
 		    printf(".\n");
-		if (!searchuser(ptr, ptr))
-		    mv_user_home(ptr);
-		else
-		    del_file(ptr);
+
+		if (!searchuser(userid, NULL)) {
+		    // user doesn't exist
+		    mv_user_home(userid);
+		} else
+		    del_file(userid);
 	    }
 	}
 	closedir(dirp);
