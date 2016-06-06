e04762e40065863837b1e46f17fa0be1078fa3c0
Kuang-che Wu <kcwu@csie.org>
2016-06-06 20:25:47 +0800

    revise path handling

--- util/mailangel.c
+++ util/mailangel.c
@@ -15,6 +15,10 @@ void sendResult();
 void mailUser(char *userid);
 
 int main(int argc, char* argv[]){
+    if (chdir(BBSHOME) != 0) {
+	perror("chdir");
+	return 1;
+    }
     if (argc < 2) {
 	fprintf(stderr, "Usage: %s file [userid]\n", argv[0]);
 	exit(1);
@@ -48,7 +52,7 @@ void readData(){
 
     attach_SHM();
 
-    fp = fopen(BBSHOME "/.PASSWDS", "rb");
+    fp = fopen(FN_PASSWD, "rb");
     j = count = 0;
     while (fread(&user, sizeof(userec_t), 1, fp) == 1) {
 	++j; /* j == uid */
@@ -85,7 +89,8 @@ void mailUser(char *userid)
     time4_t t;
     fileheader_t header;
     struct stat st;
-    char filename[512];
+    char filename[PATHLEN];
+    char buf[512];
 
     fp2 = fopen(mailfile, "r");
     if (fp2 == NULL) {
@@ -93,7 +98,7 @@ void mailUser(char *userid)
 	return;
     }
 
-    sprintf(filename, BBSHOME "/home/%c/%s", userid[0], userid);
+    sethomepath(filename, userid);
     if (stat(filename, &st) == -1) {
 	if (Mkdir(filename) == -1) {
 	    fprintf(stderr, "mail box create error %s \n", filename);
@@ -121,15 +126,15 @@ void mailUser(char *userid)
 	    "時間: %s\n",
 	    ctime4(&t));
 
-    while ((count = fread(filename, 1, sizeof(filename), fp2))) {
-	fwrite(filename, 1, count, fp);
+    while ((count = fread(buf, 1, sizeof(buf), fp2))) {
+	fwrite(buf, 1, count, fp);
     }
     fclose(fp);
     fclose(fp2);
 
     strcpy(header.title, "給小天使的一封信");
     strcpy(header.owner, "小天使系統");
-    sprintf(filename, BBSHOME "/home/%c/%s/.DIR", userid[0], userid);
+    sethomedir(filename, userid);
     append_record(filename, &header, sizeof(header));
     mailalertuser(userid);
     printf("%s\n", userid);
