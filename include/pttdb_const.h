8fe44f4772da64efef95feb1b79fa6228888525e
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-04-08 07:12:01 +0800

    pttdb:     pttdb_main:         create_main_from_fd         len_main         len_main_by_aid         n_line_main         n_line_main_by_aid         read_main_header         read_main_header_by_aid         delete_main         delete_main_by_aid         update_main_from_fd         read_main_header_to_bson         serialize_main_bson         deserialize_main_bson         serialize_update_main_bson
        pttdb_content_block:
            split_contents
            split_contents_from_fd
            delete_content
            reset_content_block
            reset_content_block_buf_block
            init_content_block_with_buf_block
            init_content_block_buf_block
            destroy_content_block
    
            associate_content_block
            dissociate_content_block
    
            save_content_block
            read_content_block
    
            read_content_blocks
            read_content_blocks_by_ref
    
            dynamic_read_content_blocks
            dynamic_read_content_blocks_by_ref
    
            read_content_blocks_to_bsons
    
            serialize_content_block_bson
            deserialize_content_block_bson
    
            sort_b_content_blocks_by_block_id
            ensure_b_content_blocks_block_ids
    
        pttdb_util:
            get_line_from_buf

--- include/pttdb_const.h
+++ include/pttdb_const.h
@@ -7,6 +7,12 @@ extern "C" {
 #endif
 
 #define MAX_BUF_SIZE 8192
+#define MAX_CREATE_MILLI_TIMESTAMP 9999999999999 // XXX 2286-11-20
+
+enum LiveStatus {
+    LIVE_STATUS_ALIVE,
+    LIVE_STATUS_DELETED,
+};
 
 #ifdef __cplusplus
 }
---
+++ include/pttdb_main_private.h
@@ -0,0 +1,17 @@
+/* $Id$ */
+#ifndef PTTDB_MAIN_PRIVATE_H
+#define PTTDB_MAIN_PRIVATE_H
+
+#include "ptterr.h"
+#include "pttdb_const.h"
+#include "pttdb_content_block.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PTTDB_MAIN_PRIVATE_H */
