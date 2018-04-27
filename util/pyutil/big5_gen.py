58a9f620afcfa48e37150e39eeb18a5a27da4be2
holishing <holishingtest@gmail.com>
2018-04-27 21:13:19 +0800

    big5: let python script both compatible with python2 and python3 1. print() : change usage form `print "string"` to `print ("string")` 2. print() : change usage for some string not end with '\n', and add `from __future__ import print_function` to let it compatible with python2. 3. let byte-like format can also be striped. 4. let range() format be converted to list() format

--- util/pyutil/big5_gen.py
+++ util/pyutil/big5_gen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Usage: ./big5_gen.py > big5_tbl.py
 
+from __future__ import print_function
 import os
 import tarfile
 
@@ -12,31 +13,31 @@ U2B_FILE = BIG5_DATA.extractfile('uao250-u2b.big5.txt')
 
 # b2u
 b2u = B2U_FILE.readlines()
-b2u = [line.strip().split(' ')
+b2u = [line.strip().split(b' ')
        for line in b2u
-       if line.strip().startswith('0x')]
+       if line.strip().startswith(b'0x')]
 b2u = dict((int(b, 0), int(u, 0)) for (b, u) in b2u)
 
-print """#!/usr/bin/env python
+print ("""#!/usr/bin/env python
 
-"""
-print "b2u_table = ("
+""")
+print ("b2u_table = (")
 for i in range(0x10000):
-    print '0x%04x,' % (i if i not in b2u else b2u[i]),
+    print ('0x%04x,' % (i if i not in b2u else b2u[i]), end=' ')
     if i % 10 == 9:
-        print ''
-print ")\n"
+        print ('')
+print (")\n")
 
 # u2b
 u2b = U2B_FILE.readlines()
-u2b = [line.strip().split(' ')
+u2b = [line.strip().split(b' ')
        for line in u2b
-       if line.strip().startswith('0x')]
+       if line.strip().startswith(b'0x')]
 u2b = dict((int(u, 0), int(b, 0)) for (b, u) in u2b)
 
-print "u2b_table = ("
+print ("u2b_table = (")
 for i in range(0x10000):
-    print '0x%04x,' % (i if i not in u2b else u2b[i]),
+    print ('0x%04x,' % (i if i not in u2b else u2b[i]), end=' ')
     if i % 10 == 9:
-        print ''
-print ")\n"
+        print ('')
+print (")\n")
