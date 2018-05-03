422cadfa17e5f8865f3002df998911d0156cb201
holishing <holishingtest@gmail.com>
2018-05-03 14:13:58 +0800

    big5: let python script both compatible with python2 and python3 (#44)
    * big5: let python script both compatible with python2 and python3
    1. print() : change usage form `print "string"` to `print ("string")`
    2. print() : change usage for some string not end with '\n', and add `from __future__ import print_function` to let it compatible with python2.
    3. let byte-like format can also be striped.
    4. let range() format be converted to list() format
    
    * big5: remove the space between 'print' and '('
    let them more like function calls.

--- common/sys/big5_gen.py
+++ common/sys/big5_gen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Usage: ./big5_gen.py > big5.c
 
+from __future__ import print_function
 import sys
 import tarfile
 
@@ -12,37 +13,37 @@ AMBCJK_FILE = BIG5_DATA.extractfile('ambcjk.big5.txt')
 # b2u
 sys.stderr.write('Generating B2U data...\n')
 b2u = B2U_FILE.readlines()
-b2u = [line.strip().split(' ')
+b2u = [line.strip().split(b' ')
        for line in b2u
-       if line.strip().startswith('0x')]
+       if line.strip().startswith(b'0x')]
 b2u = dict((int(b, 0), int(u, 0)) for (b, u) in b2u)
 
-print """#include <stdint.h>
+print("""#include <stdint.h>
 extern const uint16_t b2u_table[];
 extern const uint16_t u2b_table[];
 extern const uint8_t b2u_ambiguous_width[];
-"""
-print "const uint16_t b2u_table[0x10000] = {"
+""")
+print("const uint16_t b2u_table[0x10000] = {")
 for i in range(0x10000):
-    print '0x%04x,' % (i if i not in b2u else b2u[i]),
+    print( '0x%04x,' % (i if i not in b2u else b2u[i]), end=' ')
     if i % 10 == 9:
-        print ''
-print "};\n"
+        print('')
+print("};\n")
 
 # u2b
 sys.stderr.write('Generating U2B data...\n')
 u2b = U2B_FILE.readlines()
-u2b = [line.strip().split(' ')
+u2b = [line.strip().split()
        for line in u2b
-       if line.strip().startswith('0x')]
+       if line.strip().startswith(b'0x')]
 u2b = dict((int(u, 0), int(b, 0)) for (b, u) in u2b)
 
-print "const uint16_t u2b_table[0x10000] = {"
+print("const uint16_t u2b_table[0x10000] = {")
 for i in range(0x10000):
-    print '0x%04x,' % (i if i not in u2b else u2b[i]),
+    print( '0x%04x,' % (i if i not in u2b else u2b[i]), end=' ')
     if i % 10 == 9:
-        print ''
-print "};\n"
+        print('')
+print("};\n")
 
 # ambiguous cjk width
 sys.stderr.write('Generating AMBCJK data...\n')
@@ -52,14 +53,14 @@ for entry in ambcjk_data:
     (a, b) = entry.strip().split()
     a = int(a, 0)
     b = int(b, 0)
-    ambcjk = ambcjk + range(a, b+1)
-print "const uint8_t b2u_ambiguous_width[0x10000] = {"
+    ambcjk = ambcjk + list( range(a, b+1) )
+print("const uint8_t b2u_ambiguous_width[0x10000] = {")
 for i in range(0, 0x10000):
     if i in b2u and b2u[i] in ambcjk:
-        print "1,",
+        print("1,",end=' '),
     else:
-        print "0,",
+        print("0,",end=' '),
     if i % 25 == 24:
-        print ''
+        print('')
 
-print "};\n"
+print("};\n")
