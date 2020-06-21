d905f924c6dd27c102769470ece6d312e86d434e
Robert Wang <robertabcd@gmail.com>
2020-06-20 18:28:47 -0700

    emaildb: Move out common regmaild transaction code

--- mbbsd/emaildb.c
+++ mbbsd/emaildb.c
@@ -2,12 +2,28 @@
 #include "bbs.h"
 #include "daemons.h"
 
+static
+int regmail_transact(const void *in, size_t inlen, void *out, size_t outlen)
+{
+    int fd = toconnect(REGMAILD_ADDR);
+    if (fd < 0)
+        return -1;
+
+    if (towrite(fd, in, inlen) != inlen ||
+        toread(fd, out, outlen) != outlen)
+    {
+        close(fd);
+        return -1;
+    }
+
+    return 0;
+}
+
 #ifdef USE_EMAILDB
 
 int emaildb_check_email(const char * userid, const char * email)
 {
     int count = -1;
-    int fd = -1;
     regmaildb_req req = {0};
 
     // initialize request
@@ -16,23 +32,8 @@ int emaildb_check_email(const char * userid, const char * email)
     strlcpy(req.userid, userid, sizeof(req.userid));
     strlcpy(req.email,  email,  sizeof(req.email));
 
-    if ( (fd = toconnect(REGMAILD_ADDR)) < 0 )
-    {
-        // perror("toconnect");
+    if (regmail_transact(&req, sizeof(req), &count, sizeof(count)) < 0)
         return -1;
-    }
-
-    if (towrite(fd, &req, sizeof(req)) < 0) {
-        // perror("towrite");
-        close(fd);
-        return -1;
-    }
-
-    if (toread(fd, &count, sizeof(count)) < 0) {
-        // perror("toread");
-        close(fd);
-        return -1;
-    }
 
     return count;
 }
@@ -40,7 +41,6 @@ int emaildb_check_email(const char * userid, const char * email)
 int emaildb_update_email(const char * userid, const char * email)
 {
     int result = -1;
-    int fd = -1;
     regmaildb_req req = {0};
 
     // initialize request
@@ -49,23 +49,8 @@ int emaildb_update_email(const char * userid, const char * email)
     strlcpy(req.userid, userid, sizeof(req.userid));
     strlcpy(req.email,  email,  sizeof(req.email));
 
-    if ( (fd = toconnect(REGMAILD_ADDR)) < 0 )
-    {
-        // perror("toconnect");
+    if (regmail_transact(&req, sizeof(req), &result, sizeof(result)) < 0)
         return -1;
-    }
-
-    if (towrite(fd, &req, sizeof(req)) < 0) {
-        // perror("towrite");
-        close(fd);
-        return -1;
-    }
-
-    if (toread(fd, &result, sizeof(result)) < 0) {
-        // perror("toread");
-        close(fd);
-        return -1;
-    }
 
     return result;
 }
@@ -78,7 +63,6 @@ int emaildb_update_email(const char * userid, const char * email)
 int regcheck_ambiguous_userid_exist(const char *userid)
 {
     int result = -1;
-    int fd = -1;
     regmaildb_req req = {0};
 
     // initialize request
@@ -87,23 +71,8 @@ int regcheck_ambiguous_userid_exist(const char *userid)
     strlcpy(req.userid, userid, sizeof(req.userid));
     strlcpy(req.email,  "ambiguous@check.non-exist",  sizeof(req.email));
 
-    if ( (fd = toconnect(REGMAILD_ADDR)) < 0 )
-    {
-        // perror("toconnect");
+    if (regmail_transact(&req, sizeof(req), &result, sizeof(result)) < 0)
         return -1;
-    }
-
-    if (towrite(fd, &req, sizeof(req)) < 0) {
-        // perror("towrite");
-        close(fd);
-        return -1;
-    }
-
-    if (toread(fd, &result, sizeof(result)) < 0) {
-        // perror("toread");
-        close(fd);
-        return -1;
-    }
 
     return result;
 }
