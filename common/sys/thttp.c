88a445273c11990161fc75b8cd15b5f9fc7505e4
Robert Wang <robertabcd@gmail.com>
2021-04-02 15:46:53 -0700

    thttp: Add support for POST

--- common/sys/thttp.c
+++ common/sys/thttp.c
@@ -95,21 +95,52 @@ static int read_response(THTTP *t) {
     if (sscanf(start, "%*s%d", &t->code) != 1)
         return -1;
 
+    char *headers_start;
     char *content_start;
     if (t->code != 200) {
         // although there can be content when error,
         // we still ignore it.
         t->content_at = -1;
         t->content_length = -1;
-    } else if ((content_start = strstr(start, "\r\n\r\n")) != NULL) {
+    } else {
+	headers_start = strstr(start, "\r\n");
+	content_start = strstr(start, "\r\n\r\n");
+	if (headers_start == NULL || content_start == NULL)
+	    return -1;
+	t->headers_at = headers_start + 2 - start;
         t->content_at = content_start + 4 - start;
         t->content_length = buffer_length(b) - t->content_at - 1; // added '\0'
-    } else {
-        // invalid response
-        return -1;
     }
 
     return buffer_length(b) - 1;
+}
+
+static int thttp_connect(THTTP *t, const char *addr) {
+    t->fd = toconnect3(addr, 0, t->timeout_connect);
+    if (t->fd < 0)
+        return -1;
+
+    if (t->timeout_read > 0) {
+        // set the io timeout
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = t->timeout_read;
+        setsockopt(t->fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+    }
+
+    t->read = _read;
+    t->write = _write;
+    return 0;
+}
+
+static int thttp_read_response_and_finish(THTTP *t) {
+    if (t->failed || read_response(t) < 0) {
+        close(t->fd);
+        return -1;
+    }
+
+    close(t->fd);
+    return 0;
 }
 
 
@@ -137,34 +168,41 @@ void thttp_set_io_timeout(THTTP *t, int microsecond) {
 }
 
 int thttp_get(THTTP *t, const char *addr, const char *uri, const char *host) {
-    t->fd = toconnect3(addr, 0, t->timeout_connect);
-    if (t->fd < 0)
-        return -1;
+    int ret = thttp_connect(t, addr);
+    if (ret < 0)
+	return ret;
 
-    if (t->timeout_read > 0) {
-        // set the io timeout
-        struct timeval tv;
-        tv.tv_sec = 0;
-        tv.tv_usec = t->timeout_read;
-        setsockopt(t->fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-    }
+    ret = request(t, "GET", uri, "ssss",
+		  "Accept", "text/plain",
+	    	  "Host", host,
+	    	  "Connection", "close",
+	    	  "User-Agent", "pttbbs");
+    if (ret < 0)
+	t->failed = 1;
 
-    t->read = _read;
-    t->write = _write;
+    return thttp_read_response_and_finish(t);
+}
 
-    request(t, "GET", uri, "ssss",
-            "Accept", "text/plain",
-            "Host", host,
-            "Connection", "close",
-            "User-Agent", "pttbbs");
+int thttp_post(THTTP *t, const char *addr, const char *uri, const char *host,
+	       const char *content_type, const void *content,
+	       int content_length) {
+    int ret = thttp_connect(t, addr);
+    if (ret < 0)
+	return ret;
 
-    if (t->failed || read_response(t) < 0) {
-        close(t->fd);
-        return -1;
-    }
+    ret = request(t, "POST", uri, "sssssi",
+		  "Accept", "text/plain",
+	    	  "Host", host,
+	    	  "Connection", "close",
+	    	  "User-Agent", "pttbbs",
+		  "Content-Type", content_type,
+		  "Content-Length", content_length);
+    if (ret == 0)
+	ret = t->write(t, content, content_length);
+    if (ret < 0)
+	t->failed = 1;
 
-    close(t->fd);
-    return 0;
+    return thttp_read_response_and_finish(t);
 }
 
 int thttp_code(THTTP *t) {
@@ -177,4 +215,25 @@ void *thttp_get_content(THTTP *t) {
 
 int thttp_content_length(THTTP *t) {
     return t->code == 200 ? t->content_length : 0;
+}
+
+int thttp_get_header(THTTP *t, const char *name, const char **valptr) {
+    int namelen = strlen(name);
+    const char *s = buffer_get(&t->bresp, t->headers_at);
+    for (;;) {
+	const char *e = strstr(s, "\r\n");
+	if (e == NULL || s == e)
+	    break;  /* invalid (NULL), or end of headers. */
+
+	const char *v = s + namelen;
+	if (v + 2 <= e &&
+	    !strncasecmp(s, name, namelen) &&
+	    v[0] == ':' && v[1] == ' ') {
+	    *valptr = v + 2;
+	    return e - *valptr;
+	}
+
+	s = e + 2;
+    }
+    return -1;
 }
--- include/thttp.h
+++ include/thttp.h
@@ -8,6 +8,7 @@ typedef struct THTTP {
     int (*write)(struct THTTP *t, const void *buffer, int size);
     BUFFER bresp;
     int code;
+    int headers_at;
     int content_at;
     int content_length;
     int failed;
@@ -20,8 +21,12 @@ int thttp_cleanup(THTTP *t);
 void thttp_set_connect_timeout(THTTP *t, int microsecond);
 void thttp_set_io_timeout(THTTP *t, int microsecond);
 int thttp_get(THTTP *t, const char *addr, const char *uri, const char *host);
+int thttp_post(THTTP *t, const char *addr, const char *uri, const char *host,
+	       const char *content_type, const void *content,
+	       int content_length);
 int thttp_code(THTTP *t);
 void *thttp_get_content(THTTP *t);
 int thttp_content_length(THTTP *t);
+int thttp_get_header(THTTP *t, const char *name, const char **valptr);
 
 #endif
