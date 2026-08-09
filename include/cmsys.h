ptt/pttbbs

commit e409e6c108f997b7a71651d29dd1e65447132d32

fix(cmsys): no treating 'c' input as 'c++' when in C++ mode
clang does not allow __builtin_types_compatible_p in C++, so we should
provide the C++ version of static type checking.

include/cmsys.h
@@ -199,7 +199,10 @@ void must_getrandom(void *buf, size_t len);
 
 #ifndef STRLCPY
 #if defined(__cplusplus)
-#define MUST_BE_ARRAY(arr)
+#include <type_traits>
+#define MUST_BE_ARRAY(arr) \
+    static_assert(std::is_array<typename std::remove_reference<decltype(arr)>::type>::value, \
+                  #arr " must be a fixed-size array, not a pointer!")
 #else
 #define MUST_BE_ARRAY(arr) \
     _Static_assert(!__builtin_types_compatible_p(__typeof__(arr), __typeof__(&(arr)[0])), \

commit a98ac091d3a14ea97025b4da276ffc1252a450af

fix(cmsys): Fix compilation failure from type_traits
Give up in checking types in C++ (introduced in e409e6c1).

include/cmsys.h
@@ -199,10 +199,8 @@ void must_getrandom(void *buf, size_t len);
 
 #ifndef STRLCPY
 #if defined(__cplusplus)
-#include <type_traits>
-#define MUST_BE_ARRAY(arr) \
-    static_assert(std::is_array<typename std::remove_reference<decltype(arr)>::type>::value, \
-                  #arr " must be a fixed-size array, not a pointer!")
+// No good way to check array type in C++.
+#define MUST_BE_ARRAY(arr)
 #else
 #define MUST_BE_ARRAY(arr) \
     _Static_assert(!__builtin_types_compatible_p(__typeof__(arr), __typeof__(&(arr)[0])), \