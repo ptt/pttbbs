d6617e96b41bec51f988bf90f6dd626d3213eb49
Hung-Te Lin (piaip) <hungte@gmail.com>
2026-08-18 16:15:40 +0800

    api(mail): Add keep_copy to put a copy in mailbox
    To support 'failed to post' in future.

--- include/proto.h
+++ include/proto.h
@@ -392,6 +392,7 @@ int mail_reply(int ent, fileheader_t *fhdr, const char *direct);
 int bsmtp(const char *fpath, const char *title, const char *rcpt, const char *from);
 int notify_password_change(const char *userid, const char *email);
 void hold_mail(const char *fpath, const char *receiver, const char *title);
+void keep_copy(const char *fpath, const char *title);
 void m_init(void);
 int chkmailbox(void);
 int chkmailbox_hard_limit(void);
